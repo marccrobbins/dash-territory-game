da2c1de444971a36fe1ff68d25069b20890bb5e0
Marc <marccrobbins@gmail.com>
2020-03-10 22:48:20 -0400

    Simple move working

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    public float inputBuffer = 0.1f;
-    public float moveSpeed;
-    public float turnSpeed;
-    public float gravity = 9.18f;
-    [Tooltip("Is grounded check starts from the bottom of the CharacterController volume.")]
-    public float groundedCheckDistance = 0.1f;
-    public bool isGrounded;
+    public float moveSpeed = 6;
+    public float jumpSpeed = 8;
+    public float gravity = 20;
     
     private CharacterController _controller;
     private Vector3 _targetHeading;
@@ -22,33 +16,32 @@ public class PlayerController : MonoBehaviour
     }
 
     void Update()
-    {
-        CalculateGrounded();
-        if (!isGrounded)
+    { 
+        if (_controller.isGrounded)
         {
-            var position = transform.position;
-            position.y -= gravity * Time.deltaTime;
-            transform.position = position;
+            var h = Input.GetAxis("Horizontal");
+            var v = Input.GetAxis("Vertical");
+            
+            _targetHeading = new Vector3(h, 0, v);
+            _targetHeading *= moveSpeed;
+
+            if (Input.GetButtonUp("Fire1"))
+            {
+                _targetHeading.y = jumpSpeed;
+            }
+        }
+        
+        //Apply rotation
+        if (_targetHeading != Vector3.zero)
+        {
+            var turn = Quaternion.LookRotation(_targetHeading);
+            transform.rotation = turn;
         }
 
-        var h = Input.GetAxis("Horizontal");
-        var v = Input.GetAxis("Vertical");
-
-        _targetHeading = new Vector3(h, 0, v);
-        if (_targetHeading == Vector3.zero) return;
-        var heading = Quaternion.LookRotation(_targetHeading);
-        transform.rotation = heading;
-        _controller.Move(_targetHeading * moveSpeed);
-    }
-
-    private void CalculateGrounded()
-    {
-        var playerTransform = transform;
-        var playerPosition = playerTransform.position;
-        var height = _controller.height <= 1 ? 1 : _controller.height;
-        var rayPosition = new Vector3(playerPosition.x, playerPosition.y - height * 0.5f, playerPosition.z);
-        var ray = new Ray(rayPosition, -playerTransform.up);
+        //Apply gravity
+        _targetHeading.y -= gravity * Time.deltaTime;
         
-        isGrounded = Physics.Raycast(ray, groundedCheckDistance);
+        //Move character
+        _controller.Move(_targetHeading * Time.deltaTime);
     }
 }
