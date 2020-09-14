b7c1a1dd8dbd520218e53bd43c768fe02066be99
Marc <marccrobbins@gmail.com>
2020-09-13 20:03:23 -0400

    made a grid setup tool

---
+++ Assets/_framework/Scripts/Extensions/Vector3Extensions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Framework.Extensions
+{
+	public static class Vector3Extensions
+	{
+		public static Vector3 RotateAround(this Vector3 point, Vector3 pivot, Quaternion rotation)
+		{
+			var direction = point - pivot;
+			direction = rotation * direction;
+			return direction + pivot;
+		}
+	}
+}
---
+++ Assets/_project/Scripts/Territory/TerritoryTileManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Framework;
+using UnityEngine;
+
+namespace DashTerritory
+{
+    public class TerritoryTileManager : Manager<TerritoryTileManager>
+    {
+        public GameObject territoryTilePrefab;
+        private Dictionary<TerritoryGrid, List<TerritoryTile>> territoryTileLookup;
+        
+        protected override IEnumerator InitializeManager()
+        {
+            territoryTileLookup = new Dictionary<TerritoryGrid, List<TerritoryTile>>();
+            
+            return base.InitializeManager();
+        }
+
+        public void RegisterTerritoryGrid(TerritoryGrid grid)
+        {
+            territoryTileLookup[grid] = new List<TerritoryTile>();
+            
+            //instantiate all tiles
+            foreach (var subdivision in grid.Subdivisions)
+            {
+                var tileObject = Instantiate(territoryTilePrefab, subdivision, grid.transform.rotation, transform);
+                var territoryTile = tileObject.GetComponent<TerritoryTile>();
+                if (!territoryTile)
+                {
+                    Destroy(tileObject);
+                    continue;
+                }
+                
+                territoryTileLookup[grid].Add(territoryTile);
+            }
+        }
+    }
+}
